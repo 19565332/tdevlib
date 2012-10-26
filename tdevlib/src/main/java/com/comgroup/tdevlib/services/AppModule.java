f52b60ff2e8d46f68c935da7f21d29d181806f71
19565332 <19565332@qq.com>
2012-10-26 16:40:35 +0800

    1

--- tdevlib/src/main/java/com/comgroup/tdevlib/services/AppModule.java
+++ tdevlib/src/main/java/com/comgroup/tdevlib/services/AppModule.java
@@ -22,7 +22,6 @@ public class AppModule {
 
 	public static void bind(ServiceBinder binder){
 		// binder.bind(MyServiceInterface.class, MyServiceImpl.class);
-
 		// Make bind() calls on the binder object to define most IoC services.
 		// Use service builder methods (example below) when the implementation
 		// is provided inline, or requires more initialization than simply
